becbe671c3d1ebf19a0fdfb7e7ad0509fa1c5433
sadeghpro <sadeghmohamadnia@yahoo.com>
2021-01-30 23:04:40 +0330

    Add material theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    direction: localStorage.getItem('lang') === 'fa' ? 'rtl' : 'ltr',
+    palette: {
+        type: localStorage.getItem('mode') ? localStorage.getItem('mode') : 'light',
+    }
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );

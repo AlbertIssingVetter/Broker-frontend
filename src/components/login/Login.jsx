08374e367e192068c3f444b1c0d46ecf850d0935
sadeghpro <sadeghmohamadnia@yahoo.com>
2021-02-02 23:51:56 +0330

    Change password text field and add ref

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,20 +1,53 @@
 import React from "react";
-import {Button, Checkbox, FormControlLabel, Grid, TextField, Typography} from "@material-ui/core";
+import {
+    Button,
+    Checkbox, FormControl,
+    FormControlLabel,
+    Grid,
+    IconButton, Input,
+    InputAdornment, InputLabel,
+    TextField,
+    Typography
+} from "@material-ui/core";
 import Pattern from "./Pattern";
 import {Link} from "react-router-dom";
 import {withRouter} from 'react-router-dom'
+import {Visibility, VisibilityOff} from "@material-ui/icons";
 
 export default class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPassword: false,
+        }
+        this.inputUser = React.createRef();
+        this.inputPass = React.createRef();
+    }
+
+
+    handleShowPasswordClick = () => {
+        this.setState({
+            showPassword: !this.state.showPassword
+        })
+    }
+
+    handleLoginBtnClick = () => {
+        console.log('click')
+    }
 
     render() {
         const Signup = withRouter(({history}) => (
                 <Button style={{float: 'left'}}
-                        onClick={() => { history.push('/signup') }}
+                        onClick={() => {
+                            history.push('/signup')
+                        }}
                         variant='outlined' color='primary'>
                     sign up
                 </Button>
             )
         )
+
 
         return (
             <div className='login'>
@@ -28,10 +61,26 @@ export default class Login extends React.Component {
                             <Typography>Welcome back! Please login to your account.</Typography>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField label='username'/>
+                            <TextField label='username' ref={this.inputUser}/>
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField label='password'/>
+                            <FormControl>
+                                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                                <Input
+                                    ref={this.inputPass}
+                                    type={this.state.showPassword ? 'text' : 'password'}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onMouseDown={this.handleShowPasswordClick}
+                                            >
+                                                {this.state.showPassword ? <Visibility/> : <VisibilityOff/>}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }/>
+                            </FormControl>
+
                         </Grid>
                         <Grid item xs={12}>
                             <div className='forget-row'>
@@ -40,7 +89,7 @@ export default class Login extends React.Component {
                             </div>
                         </Grid>
                         <Grid item xs={6}>
-                            <Button style={{float: 'right'}} variant='contained' color='primary'>login</Button>
+                            <Button style={{float: 'right'}} onClick={this.handleLoginBtnClick} variant='contained' color='primary'>login</Button>
                         </Grid>
 
                         <Grid item xs={6}>

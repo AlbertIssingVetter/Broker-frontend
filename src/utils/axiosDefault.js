1716cb39a2b9c8928da8acc9e509a45824c74929
sadeghpro <sadeghmohamadnia@yahoo.com>
2021-02-08 13:09:58 +0330

    Fix authorization header after refresh the page

--- src/utils/axiosDefault.js
+++ src/utils/axiosDefault.js
@@ -3,4 +3,7 @@ import {getLang} from '../lang/t';
 
 axios.defaults.baseURL = 'http://localhost/api'
 axios.defaults.headers.common['X-lang'] = getLang();
+if (localStorage.getItem('token')) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+}
 

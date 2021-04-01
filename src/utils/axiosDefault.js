b52f9dc8a1949f405985cd216d3dd02ed373e3ee
sadeghpro <sadeghmohamadnia@yahoo.com>
2021-04-01 19:28:20 +0430

    Change domain

--- src/utils/axiosDefault.js
+++ src/utils/axiosDefault.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {getLang} from '../lang/t';
 
-axios.defaults.baseURL = 'http://localhost/api'
+axios.defaults.baseURL = 'https://trustmarket.net/api'
 axios.defaults.headers.common['X-lang'] = getLang();
 if (localStorage.getItem('token')) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;

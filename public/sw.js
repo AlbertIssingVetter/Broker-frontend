dd22f31054c8260f9642b4e17943f829cd4341a0
sadeghpro <sadeghmohamadnia@yahoo.com>
2021-04-08 14:37:40 +0430

    Add vazir font files to the project

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,5 @@
 const cacheFiles = [];
 const cacheVersion = '1.0.0';
-cacheFiles.push('https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v27.2.1/dist/font-face.css');
 cacheFiles.push('./');
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('install', e => {

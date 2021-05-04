5f34fc4ad95f0729d315dd827935fb33dcb20e9e
sadeghpro <sadeghmohamadnia@yahoo.com>
2021-05-04 14:48:52 +0430

    Fix transaction history after use mysql instead of elasticsearch

--- src/components/wallet/TransactionTableHistory.jsx
+++ src/components/wallet/TransactionTableHistory.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import {withRouter} from 'react-router-dom';
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip} from "@material-ui/core";
 import t, {getLang} from "../../lang/t";
 import {numberWithCommas} from "../../utils/tools";
-import {Alert} from "@material-ui/lab";
+import WarningIcon from '@material-ui/icons/Warning';
+import ErrorIcon from '@material-ui/icons/Error';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import {withTheme} from "@material-ui/core/styles";
 
 class TransactionTableHistory extends React.Component {
 
@@ -16,6 +18,7 @@ class TransactionTableHistory extends React.Component {
                             <TableCell align="center">{t('date')}</TableCell>
                             <TableCell align="center">{t('address')}</TableCell>
                             <TableCell align="center">{t('transactionVolume')}</TableCell>
+                            <TableCell align="center">{t('status')}</TableCell>
                             <TableCell align="center">{t('description')}</TableCell>
                         </TableRow>
                     </TableHead>
@@ -23,26 +26,38 @@ class TransactionTableHistory extends React.Component {
                         {this.props.transactions.map((transaction) => (
                             <TableRow key={transaction.date}>
                                 <TableCell align="center" component="th" scope="row">
-                                    {new Date(transaction.date * 1000)
+                                    {new Date(transaction.date)
                                         .toLocaleString(getLang() === 'fa' ? 'fa' : 'en-US')}
                                 </TableCell>
                                 <TableCell align="center">{transaction.address}</TableCell>
                                 <TableCell align="center">{numberWithCommas(transaction.amount)}</TableCell>
                                 <TableCell align="center">
                                     {
-                                        ((transaction.status === 0 || transaction.status > 1) && Date.now() - 240 * 60 < transaction.date)
-                                        && (<Alert severity='warning'>{t('paymentWaiting')}</Alert>)
+                                        ((transaction.status === "0" || transaction.status === "2" || transaction.status === "-2") && Date.now() - 240 * 60 < transaction.date)
+                                        && (
+                                            <Tooltip title={t('paymentWaiting')}>
+                                                <WarningIcon style={{color: this.props.theme.palette.warning.main}}/>
+                                            </Tooltip>
+                                        )
                                     }
 
                                     {
-                                        (transaction.status < 0 || ((transaction.status === 0 || transaction.status > 1) && Date.now() - 240 * 60 >= transaction.date))
-                                        && (<Alert severity='error'>{t('paymentError')}</Alert>)
+                                        (transaction.status === "-1" || ((transaction.status === "0" || transaction.status === "2") && Date.now() - 240 * 60 >= transaction.date))
+                                        && (
+                                            <Tooltip title={t('paymentError')}>
+                                                <ErrorIcon  style={{color: this.props.theme.palette.error.main}}/>
+                                            </Tooltip>
+                                        )
                                     }
 
                                     {
-                                        transaction.status === 1 && (<Alert severity='success'>{t('paymentSuccess')}</Alert>)
+                                        transaction.status === "1" && (
+                                            <Tooltip title={t('paymentSuccess')}>
+                                                <CheckCircleIcon  style={{color: this.props.theme.palette.success.main}}/>
+                                            </Tooltip>)
                                     }
                                 </TableCell>
+                                <TableCell align="center">{transaction.description}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
@@ -52,4 +67,4 @@ class TransactionTableHistory extends React.Component {
     }
 }
 
-export default withRouter(TransactionTableHistory);
+export default withTheme(TransactionTableHistory);

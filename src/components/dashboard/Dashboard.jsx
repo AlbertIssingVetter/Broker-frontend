06da6c2f7dc012761e5de2eb4b4e50504753ce1c
sadeghpro <sadeghmohamadnia@yahoo.com>
2021-05-05 21:26:49 +0430

    Get dashboard data

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,19 +1,46 @@
 import React from "react";
 import {withRouter} from 'react-router-dom';
 import {Grid} from "@material-ui/core";
-import coins from "../../utils/coins";
 import YourWallet from "./YourWallet";
 import AccountStatus from "./AccountStatus";
 import DashboardCoins from "./DashboardCoins";
+import axios from "axios";
 
 class Dashboard extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            coins,
-            selected: 'btc'
+            data: {},
         }
+    }
+
+    componentDidMount() {
+        this.getDashboardData();
+        this.getMarketDataInterval = setInterval(this.getDashboardData, 5000);
+    }
+
+
+
+    componentWillUnmount() {
+        clearInterval(this.getMarketDataInterval);
+    }
+
+    getDashboardData = () => {
+        axios({
+            url: '/dashboard',
+            method: 'POST',
+        }).then(res => {
+            if (res.data.status) {
+                this.setState({
+                    data: res.data.data
+                })
+            } else {
+                console.log('error')
+            }
+        }).catch(err => {
+            console.log(err);
+        });
     }
 
     render() {
